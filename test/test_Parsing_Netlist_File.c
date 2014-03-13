5918d2b227b9d52e1f96902c2246d19244a9959d
Francis Sowani <frathoso@gmail.com>
2014-03-13 19:57:56 +0400

    TEST - Add circuit file parsing tests

--- test/test_Parsing_Netlist_File.c
+++ test/test_Parsing_Netlist_File.c
@@ -31,6 +31,8 @@ void test_create_empty_circuit();
 void test_append_a_gate_into_a_circuit();
 void test_search_gate_index_in_a_circuit();
 
+void test_populate_circuit_from_an_input_file();
+
 
 /*
  *	Run all tests
@@ -44,6 +46,8 @@ void test_fixture_Parsing_Netlist_File( void )
 	run_test(test_create_empty_circuit);
 	run_test(test_append_a_gate_into_a_circuit);
 	run_test(test_search_gate_index_in_a_circuit);
+
+	run_test(test_populate_circuit_from_an_input_file);
 
 	// Ends a fixture
 	test_fixture_end();                 
@@ -96,4 +100,69 @@ void test_search_gate_index_in_a_circuit()
 	assert_int_equal(index, 0);
 
 	if(circuit[0]) free(circuit[0]);
+}
+
+/*		*/
+void test_populate_circuit_from_an_input_file()
+{
+	#define FILE_NAME 					"benchmarks/test-0.bench"
+	#define TOTAL_LINES 				9
+	#define TOTAL_PRIMARY_INPUT_LINES	4
+	#define TOTAL_PRIMARY_OUTPUT_LINES	1
+
+	CIRCUIT circuit;
+	CIRCUIT_INFO info;
+
+	populateCircuit(circuit, &info, FILE_NAME);
+
+	// Test for circuit statistics
+	assert_int_equal(info.numGates, TOTAL_LINES);
+	assert_int_equal(info.numPI, 	TOTAL_PRIMARY_INPUT_LINES);
+	assert_int_equal(info.numPO, 	TOTAL_PRIMARY_OUTPUT_LINES);
+
+	// Test for gate types flags
+	int index_NOT = findIndex(circuit, &info.numGates, "J", FALSE);
+	int index_AND = findIndex(circuit, &info.numGates, "H", FALSE);
+	int index_OR  = findIndex(circuit, &info.numGates, "K", FALSE);
+	
+	assert_true((circuit[index_NOT]->type == BUF && circuit[index_NOT]->inv == TRUE));
+	assert_true((circuit[index_AND]->type == AND && circuit[index_AND]->inv == FALSE));
+	assert_true((circuit[index_OR]->type  == OR  && circuit[index_OR]->inv == FALSE));
+
+	// Test for primary output and output flags
+	int index_PI = findIndex(circuit, &info.numGates, "B", FALSE);
+	int index_PO = findIndex(circuit, &info.numGates, "L", FALSE);
+
+	assert_true((circuit[index_PI]->type == PI));
+	assert_true((circuit[index_PO]->PO == TRUE));
+
+	// Test for input and output lists to a gate
+	#define TOTAL_INPUT_LINES 	2
+	int index_IN   = findIndex(circuit, &info.numGates, "H", FALSE);
+	int index_IN_1 = findIndex(circuit, &info.numGates, "A", FALSE);
+	int index_IN_2 = findIndex(circuit, &info.numGates, "B", FALSE);
+	int in_list[MAX_INPUT_GATES] = {index_IN_1, index_IN_2};
+
+	assert_n_array_equal(in_list, circuit[index_IN]->in, TOTAL_INPUT_LINES);
+
+	#define TOTAL_OUTPUT_LINES 	2
+	int index_OUT   = findIndex(circuit, &info.numGates, "B", FALSE);
+	int index_OUT_1 = findIndex(circuit, &info.numGates, "H", FALSE);
+	int index_OUT_2 = findIndex(circuit, &info.numGates, "I", FALSE);
+	int out_list[MAX_INPUT_GATES] = {index_IN_1, index_IN_2};
+
+	assert_n_array_equal(out_list, circuit[index_IN]->in, TOTAL_OUTPUT_LINES);
+
+	// Test for gate names flags
+	char* gate_names[] = {"A", "B", "C", "E", "L", "J", "I", "H", "K"};
+	int K = 0;
+
+	for(; K < TOTAL_LINES; K++)
+	{
+		assert_string_equal(gate_names[K], circuit[K]->name);
+	}
+
+	// Clear memories
+	for(K = 0; K < TOTAL_LINES; K++)
+		if(circuit[K]) free(circuit[K]);
 }
